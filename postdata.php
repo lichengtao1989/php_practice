5eb60332575f4e2c883a86c06f2d23917d4a0780
lichengtao <372425159@qq.com>
2016-10-27 14:54:44 +0800

    php

---
+++ postdata.php
@@ -0,0 +1,7 @@
+<?php
+$d1=$_POST['data1'];
+$d2=$_POST['data1'];
+echo "d1=$d1,d2=$d2";
+echo "<hr/>";
+var_dump($_POST);
+?>
---
+++ test.php
@@ -0,0 +1,6 @@
+<?php
+echo " 当前时间 ： ".date("Y-m-d H:i:s");
+echo "</br>";
+// $LINK=mysqli_connect("localhost","root","wo2095296");
+// var_dump($LINK)
+?>
